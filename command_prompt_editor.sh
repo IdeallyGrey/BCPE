32d105a9060dfb9562ed1b1ec30b347edf9db960
Lukas Hamm <ideallygrey@tuta.io>
2021-07-26 21:37:09 -0400

    added start confirmation

---
+++ command_prompt_editor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# An editor to create a custom command prompt.
+
+
+easy_edit_form=""
+expanded_form=""
+
+echo "Running this editor will clear your shell, do you want to continue? (y/n)"
+read start_confirm
+if [ "$start_confirm" != "y" ] && [ "$start_confirm" != "Y" ]; then
+echo "Quiting..."; exit
+else
+
+
+clear
+echo "
+--- Command Prompt Editor ---"
+
+fi
